242489a99bde1dc01e971aa16b425deb846e1898
Arpackit <arpackit@gmail.com>
2024-01-02 14:22:53 +0200

    add _component.kt component

---
+++ app/src/main/java/com/arpackit/arfacator/ui/component/_component.kt
@@ -0,0 +1,12 @@
+package com.arpackit.arfacator.ui.component
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+import androidx.compose.ui.unit.dp
+
+
+val appRoundedShape = RoundedCornerShape(
+    topStart = 8.dp,
+    topEnd = 12.dp,
+    bottomStart = 12.dp,
+    bottomEnd = 8.dp)
