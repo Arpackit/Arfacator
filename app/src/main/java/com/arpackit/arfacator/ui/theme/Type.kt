33202ea60b25c6fac95614ab0118c73b3aede66d
Arpackit <arpackit@gmail.com>
2024-01-02 16:27:33 +0200

    add styles & use custom fonts

--- app/src/main/java/com/arpackit/arfacator/ui/theme/Type.kt
+++ app/src/main/java/com/arpackit/arfacator/ui/theme/Type.kt
@@ -2,27 +2,64 @@ package com.arpackit.arfacator.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
+import com.arpackit.arfacator.R
+
+
+fun Typography.defaultFontFamily(fontFamily: FontFamily): Typography {
+    return this.copy(
+        displayLarge = this.displayLarge.copy(fontFamily = fontFamily),
+        displayMedium = this.displayMedium.copy(fontFamily = fontFamily),
+        displaySmall = this.displaySmall.copy(fontFamily = fontFamily),
+        headlineLarge = this.headlineLarge.copy(fontFamily = fontFamily),
+        headlineMedium = this.headlineMedium.copy(fontFamily = fontFamily),
+        headlineSmall = this.headlineSmall.copy(fontFamily = fontFamily),
+        titleLarge = this.titleLarge.copy(fontFamily = fontFamily),
+        titleMedium = this.titleMedium.copy(fontFamily = fontFamily),
+        titleSmall = this.titleSmall.copy(fontFamily = fontFamily),
+        bodyLarge = this.bodyLarge.copy(fontFamily = fontFamily),
+        bodyMedium = this.bodyMedium.copy(fontFamily = fontFamily),
+        bodySmall = this.bodySmall.copy(fontFamily = fontFamily),
+        labelLarge = this.labelLarge.copy(fontFamily = fontFamily),
+        labelMedium = this.labelMedium.copy(fontFamily = fontFamily),
+        labelSmall = this.labelSmall.copy(fontFamily = fontFamily)
+    )
+}
+
+val DeliusFamily = FontFamily(Font(R.font.delius))
+val PoppinsFamily = FontFamily(Font(R.font.poppins))
+
 val Typography = Typography(
-    bodyLarge = TextStyle(fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal, fontSize = 16.sp, lineHeight = 24.sp,
-        letterSpacing = 0.5.sp)/* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyLarge = TextStyle(
         fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        //fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */)
+).defaultFontFamily(PoppinsFamily)
+
+val cardTitleStyle = Typography.titleLarge.copy(
+    fontSize = 15.sp,
+    letterSpacing = 0.5.sp,
+)
+
+val cardTotpStyle = Typography.titleLarge.copy(
+    fontWeight = FontWeight.Medium,
+    fontSize = 27.sp,
+)
