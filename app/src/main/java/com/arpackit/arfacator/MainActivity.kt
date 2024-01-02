ed2345489baf0f33595ecd319740827aff2fd16f
Arpackit <arpackit@gmail.com>
2024-01-02 13:58:44 +0200

    just created, add dependencies

---
+++ app/src/main/java/com/arpackit/arfacator/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.arpackit.arfacator
+
+import android.os.Bundle
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+import androidx.navigation.compose.rememberNavController
+
+import com.arpackit.arfacator.ui.theme.MyComposeApplicationTheme
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        setContent {
+            MyComposeApplicationTheme {
+                //NavGraph(rememberNavController())
+            }
+        }
+    }
+}
